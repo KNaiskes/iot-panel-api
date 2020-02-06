f39e63672ae3226dd3fa415773655cd1f0174011
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-02-06 20:43:00 +0200

    add GET with id

--- api/routes/lights.js
+++ api/routes/lights.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
-	message: 'ligts - GET request'
+	message: 'light - GET request'
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).json({
+	message: 'lights - GET with id',
+	id: id
     });
 });
 
