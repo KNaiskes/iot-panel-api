5b1d746af429942de3b78e081c0a96fadec4afaa
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-02-12 13:50:19 +0200

    add logger - morgan

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const lightRoutes = require('./api/routes/lights');
 
+// Logger
+app.use(morgan('dev'));
+
+// Routes
 app.use('/lights', lightRoutes);
 
 module.exports = app;
